4263232b81288be3f15a79e4a408626d733f6b72
Guillem <info@disedit.com>
2017-07-26 10:14:35 +0200

    Add data in Spanish

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ module.exports = {
   entry: './src/main.js',
   output: {
     path: path.resolve(__dirname, './dist'),
-    publicPath: (process.env.NODE_ENV === 'production') '/wp-content/themes/Compromis/campaigns/accountable-gov/' ? : '/dist/',
+    publicPath: (process.env.NODE_ENV === 'production') ? '/wp-content/themes/Compromis/campaigns/accountable-gov/' : '/dist/',
     filename: 'build.js'
   },
   module: {
